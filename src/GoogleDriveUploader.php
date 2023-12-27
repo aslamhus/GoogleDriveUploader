72fcea1667cf8be63f64761dc2b77d75b6530290
aslamhus <aslam.percy.husain@gmail.com>
2023-12-27 11:21:16 -0800

    simplify phpdocblock for constructor

--- src/GoogleDriveUploader.php
+++ src/GoogleDriveUploader.php
@@ -9,87 +9,33 @@ use Google\Http\MediaFileUpload;
 
 /**
  * Google Drive Uploader
+ * v1.0.0
  *
- * by @aslamhus on github
+ * by @aslamhus
  *
- * ## Dependencies
+ * For further documentation @see https://github.com/aslamhus/GoogleDriveUploader.git
+ * or the README.md file
  *
- * Requires a google service account with the google drive api enabled
- *
- * @see https://developers.google.com/drive/api/v3/quickstart/php
- * @see https://developers.google.com/drive/api/v3/manage-uploads
- * @see https://github.com/googleapis/google-api-php-client/blob/main/examples/large-file-upload.php
- *
- *
- * ## Composer dependencies
- *
- * run composer require google/apiclient
- *
- * ## Instructions to set up Google Drive API service account
- *
- * You need a Google Cloud Console service account and credentials see (<https://developers.google.com/workspace/guides/create-project>)
- *
- * 1. create a google cloud project:
- * go to page-> <https://console.cloud.google.com/projectcreate>
- *
- * 2. enable google api you want to use
- * In the Google Cloud console, go to Menu menu > More products > Google Workspace > Product Library.
- *
- * 3. set up Oauth for your app
- *  under Apis and services, go to OAuth consent screen and follow instructions
- *
- * 4. choose scopes, for Google Drive API see scopes here: <https://developers.google.com/drive/api/guides/api-specific-auth>
- *
- * 5. create access credentials (api key)
- * In the Google Cloud console, go to Menu menu > APIs & Services > Credentials.
- *  click create credentails -> api key
- * Copy it and add it to your config file / env file
- *
- * 6. create access credentails (Oauth client id)
- * click create crednetials -> oauth client id
- * add the authorized urlis (authorized javascript origins/redirect urls)
- * Download the client secret json file
- *
- * _NOTE: you must also download the service account secret. Go tot service accounts -> keys -> create key_
- *
- * 7. create service account
- * In the Google Cloud console, go to Menu menu > IAM & Admin > Service Accounts.
- * click create service account
- *
- * 8. add service account to google drive folder (give permissions to the service account email)
- * Use the folder id of the folder you want to upload to
- *
- * You're done!
- *
- *
- * ## Usage
  *
  * ### Basic upload
- *
- * @example
- *
+ * ```php
  * $uploader = new DriveUploader($driveFolderId);
  * $uploader->uploadBasic($filePath, $fileName, $mimeType);
+ * ```
  *
  * ### Resumable upload
- *
- * @example
- *
+ * ```php
  * // init the uploader and get the resume uri
  * $resumeUri = $uploader->initResumable($fileName, $mimeType);
  * // upload file
  * $uploader->startResumable($filePath, $mimeType);
- *
+ * ```
  *
  * ### Asynchronous Resumable upload
- * By default, the startResumable() method is synchronous and will return the final response from the google drive api
- * However, you can set the async flag to true to make it asynchronous
- *
- * @example
- *
+ * ```php
  * // init the uploader and get the resume uri
  * $resumeUri = $uploader->initResumable($fileName, $mimeType);
- * // upload file asynchronously by setting the async flag to true
+ * // upload file asynchronously by setting the async flag to true in the 3rd argument
  * $asyncTask = $uploader->startResumable($filePath, $mimeType, true);
  * foreach($asyncTask as $response) {
  *      // continue any other logic
@@ -98,18 +44,17 @@ use Google\Http\MediaFileUpload;
  * }
  * // once the upload is complete it will return a Google Drive File object
  * $fileId = $response['id'];
- *
+ * ```
  *
  * ### Resume an interrupted upload
- *
- * @example
- *
+ * ```php
  * // Note: The resume URI is stored after initResumable(), and can be accessed with getResumeUri() after resumableUpload() is called
  * $uploader = new DriveUploader($driveFolderId);
  * $resumeUri = $uploader->initResumable($fileName, $mimeType);
  * $uploader->startResumable($filePath, $mimeType);
  * // at this point, if the upload is interrupted, you can resume it with the resume() method
  * $uploader->resume($resumeUri);
+ * ```
  *
  *
  */
